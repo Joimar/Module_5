131cdf2f6fc385f4f7b0bcd3edb6193f64c758c4
Joimar <joimarbgf@gmail.com>
2025-07-24 11:58:21 -0300

    implementation of EqualizerService class

--- app/src/main/java/com/example/vehicleequalizernative/MainActivity.kt
+++ app/src/main/java/com/example/vehicleequalizernative/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.vehicleequalizernative
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import android.widget.SeekBar
 import android.widget.TextView
 import com.example.vehicleequalizernative.databinding.ActivityMainBinding
@@ -11,6 +17,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val TAG = "VehicleEqualizerApp"
     private var equalizerService: IEqualizerService? = null
+    private val vehicleSensorSimulator = VehicleSensorSimulator("Velocidade") // Instância do simulador de sensorde velocidade
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service:
+        IBinder?) {
+            equalizerService =
+                IEqualizerService.Stub.asInterface(service)
+            Log.i(TAG, "Conectado ao EqualizerService.")
+            equalizerService?.setEqualizerEnabled(binding.switch1.isChecked)
+            equalizerService?.setBassLevel(binding.seekBar3.progress)
+            equalizerService?.setMidLevel(binding.seekBar2.progress)
+            equalizerService?.setTrebleLevel(binding.seekBar.progress)
+        }
+        override fun onServiceDisconnected(name: ComponentName?) {
+            equalizerService = null
+            Log.w(TAG, "Desconectado do EqualizerService.")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +52,8 @@ class MainActivity : AppCompatActivity() {
         binding.switch1.setOnCheckedChangeListener(
             {_, isChecked ->
                 setEqualizerControlsEnabled(isChecked)
-                setEqualizerEnabledNative(isChecked) // Chama a função nativa
+                equalizerService?.setEqualizerEnabled(isChecked)
+                setEqualizerEnabledNative(isChecked) // C// Chama a função nativa do Módulo 04
             }
         )
 
@@ -40,12 +65,18 @@ class MainActivity : AppCompatActivity() {
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 when (seekBar?.id) {
-                    binding.seekBar.id ->
+                    binding.seekBar.id ->{
                         setBassLevelNative(seekBar.progress)
-                    binding.seekBar2.id ->
+                        setBassLevelNative(seekBar.progress)
+                    }
+                    binding.seekBar2.id ->{
                         setMidLevelNative(seekBar.progress)
-                    binding.seekBar3.id ->
+                        setMidLevelNative(seekBar.progress)
+                    }
+                    binding.seekBar3.id ->{
                         setTrebleLevelNative(seekBar.progress)
+                        setTrebleLevelNative(seekBar.progress)
+                    }
                 }
             }
         }
@@ -55,8 +86,20 @@ class MainActivity : AppCompatActivity() {
         binding.seekBar2.setOnSeekBarChangeListener(seekBarChangeListener)
         binding.seekBar3.setOnSeekBarChangeListener(seekBarChangeListener)
 
+        // Listener para o botão de leitura de velocidade
+        binding.readSpeedButton.setOnClickListener{
+            val currentSpeed = vehicleSensorSimulator.readSensorData()
+            binding.speedLabel.text = "Velocidade Atual: $currentSpeed km/h"
+            Log.d(TAG, "Velocidade lida: $currentSpeed km/h")
+        }
     }
 
+    override fun onStart()
+    {
+        super.onStart()
+        val intent = Intent(this, IEqualizerService::class.java)
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
     /**
      * Habilita ou desabilita os controles do equalizador (SeekBars).
      * @param enabled true para habilitar, false para desabilitar.
